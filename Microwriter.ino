54721bf9250d0bdda7a60801e7471e838775dac9
VikOlliver <vik@diamondage.co.nz>
2020-01-23 15:45:22 +1300

    Correcting function key numbering
    I got "1" and " " the wrong way round.

--- Microwriter.ino
+++ Microwriter.ino
@@ -28,7 +28,7 @@ const char extraTable[] = {0, KEY_ESC, 0, 0, 0, '[', KEY_DELETE, 0,
                            0, '\\', 0, ']', KEY_PAGE_DOWN, 0, 0, 0,
                            KEY_PAGE_UP, 0, 0, 0, 0, 0, 0, 0
                           };
-const char funcTable[] = {KEY_F1, 0, KEY_F2, KEY_F10, 0, KEY_F11, KEY_F3, 0,
+const char funcTable[] = {0, KEY_F1, KEY_F2, KEY_F10, 0, KEY_F11, KEY_F3, 0,
                           0, 0, 0, 0, 0, KEY_F12, KEY_F4, KEY_F6,
                           0, 0, 0, 0, 0, 0, 0, KEY_F7,
                           0, 0, 0, KEY_F8, 0, KEY_F9, KEY_F5
